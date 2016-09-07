c4d8b70b88c549d282ba58c0732183e59cebf758
David Davis <ddavis@xtech.com>
2016-09-06 20:31:22 -0700

    Remove warnings with ios9 deployment target. Add AppTransportSecurity plist setting for ios9 which now requires https for all networking.

--- Core Data SPoT/CoreDataTableViewController.m
+++ Core Data SPoT/CoreDataTableViewController.m
@@ -116,6 +116,11 @@
             case NSFetchedResultsChangeDelete:
                 [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
                 break;
+
+            case NSFetchedResultsChangeMove:
+            case NSFetchedResultsChangeUpdate:
+                [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+                break;
         }
     }
 }
